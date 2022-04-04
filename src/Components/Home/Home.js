ce72091aa732659ad5e9da61ae16541122aa4022
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-04-04 18:01:42 +0600

    reviews page navigated clicking see all reviews button by useNavigate

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import useReview from '../hooks/useReview';
+import { useNavigate } from 'react-router-dom';
 import Reviews from '../Reviews/Reviews';
 import './Home.css';
 
+
 const Home = () => {
-    
-    
+    const navigate = useNavigate();
     return (
         <div className="">
             <div className="header-container">
@@ -25,7 +25,7 @@ const Home = () => {
 
 
                 <div className="btn-container">
-                    <button className="review-btn">
+                    <button onClick={() => navigate('/reviews')} className="review-btn">
                         <p>See All Reviews </p>
                         <svg id="arrow-icon" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
--- src/Components/hooks/useReview.js
+++ src/Components/hooks/useReview.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 
+
+
 const useReview = () => {
     const [reviews, setReviews] = useState([]);
     
