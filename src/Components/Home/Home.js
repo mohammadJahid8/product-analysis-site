becc013f632224fd4c5a12a1cb10db4fd8eab684
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-04-04 15:12:09 +0600

    header section done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import Reviews from '../Reviews/Reviews';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className="">
             <div className="header-container">
-                <div>
-                    <h1>dfghdfvui</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias neque totam nemo adipisci dolorum libero porro repellendus quo blanditiis!</p>
+                <div className='header-info'>
+                    <h1>It is better to light a candle <br /> <span id="header-title">Than curse the darkness.</span></h1>
+                    <p>It’s an exotic island escape in the beautiful mix of bright citrus, sweet florals, and spicy vanilla. Top: Citrus, Melon, Berry. Mid: Osmanthus. Base: Spicy Vanilla, Musk, Woody Notes. Top note is the initial impression of the fragrance, middle note is the main body of the scent and base is its final impression.</p>
                 </div>
                 <div>
-                    <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.20f/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7b3bbbc8/hires/026459591.jpg?sh=471&yocs=o_s_" alt="" />
+                    <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.20f/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8ec2a2fb/crop/026458604_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_" alt="" />
                 </div>
 
+
+            </div>
+            <div>
+                <h2 className="home-review">Customer Review(3)</h2>
+                <Reviews></Reviews>
+                <div className="btn-container">
+
+                    <button className="review-btn"><p>See All Reviews</p></button>
+                </div>
             </div>
         </div>
     );
